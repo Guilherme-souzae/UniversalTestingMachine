ffdae412c360f151a92c4ea9b3dc0cf90606c193
Guilherme-souzae <guilhermesouzae.contato@gmail.com>
2026-06-11 09:59:56 -0300

    refactor: botão de conexão

--- interface/frontend/tabs/configuration_tab.py
+++ interface/frontend/tabs/configuration_tab.py
@@ -1,15 +1,18 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout
 
 from frontend.widgets.manual_widget import ManualWidget
+from frontend.widgets.conection_widget import ConnectionWidget
 
 class ConfigurationTab(QWidget):
     def __init__(self):
         super().__init__()
 
         self.manual_widget = ManualWidget()
+        self.connection_widget = ConnectionWidget()
 
         layout = QVBoxLayout()
 
         layout.addWidget(self.manual_widget)
+        layout.addWidget(self.connection_widget)
 
         self.setLayout(layout)
---
+++ interface/frontend/widgets/conection_widget.py
@@ -0,0 +1,40 @@
+from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtWidgets import (
+    QWidget,
+    QGroupBox,
+    QPushButton,
+    QVBoxLayout,
+)
+
+class ConnectionWidget(QWidget):
+
+    connect_requested = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+
+        group = QGroupBox("Conexão")
+
+        self.btn_connect = QPushButton(
+            "CONECTAR AO ARDUINO"
+        )
+
+        self.btn_connect.setObjectName(
+            "connectButton"
+        )
+
+        self.btn_connect.setMinimumHeight(60)
+
+        self.btn_connect.clicked.connect(
+            self.connect_requested.emit
+        )
+
+        group_layout = QVBoxLayout()
+        group_layout.addWidget(self.btn_connect)
+
+        group.setLayout(group_layout)
+
+        layout = QVBoxLayout()
+        layout.addWidget(group)
+
+        self.setLayout(layout)
